e53515d4afe8dc85455649fc7528b8b9b7203fb4
jkis-iced <juantricolor.reis@gmail.com>
2022-11-09 11:01:01 -0300

    projeto de relogio.

---
+++ relogio.js
@@ -0,0 +1,16 @@
+
+const time = setInterval(() =>{
+
+    let dateToday = new Date();
+    let hour = dateToday.getHours();
+    let minutes = dateToday.getMinutes();
+    let seconds = dateToday.getSeconds();
+
+    if(hour < 10) hour = "0"  + hour;
+    if(minutes < 10) minutes = "0"  + minutes;
+    if(seconds < 10) seconds = "0"  + seconds;
+
+    document.getElementById("hour").innerHTML = hour
+    document.getElementById("minute").innerHTML = minutes
+    document.getElementById("seconds").innerHTML = seconds
+});
